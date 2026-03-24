b1e7fabffd7d5a505229e2360bab9388344ca724
kchun0513 <62457974+kchun0513@users.noreply.github.com>
2026-03-24 16:17:07 +0900

    15일차 컴포넌트 실습 업로드

---
+++ 03_Vue/01_basic/07_프로젝트/todolist-refactor/src/components/TodoList2.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <ul class="list-group">
+                <TodoListItem
+                    v-for="todoItem in todoList"
+                    :key="todoItem.id"
+                    :todoItem="todoItem"
+                    @delete-todo="deleteTodo"
+                    @toggle-completed="toggleCompleted"
+                />
+            </ul>
+        </div>
+    </div>
+</template>
+<script setup>
+    import TodoListItem from './TodoListItem2.vue';
+    const emit = defineEmits(['toggle', 'delete'])
+
+    const toggleCompleted = (id) => {
+        emit('toggle', id);
+    }
+    const deleteTodo = (id) => {
+        console.log(id)
+        emit('delete', id);
+    }
+    
+    const props = defineProps(
+        {
+            todoList: Array
+        }
+    )
+</script>
---
+++ 03_Vue/01_basic/07_프로젝트/todolist-refactor/src/components/TodoListItem2.vue
@@ -0,0 +1,33 @@
+<template>
+    <li 
+        class="list-group-item"
+        :class="{ 'list-group-item-success': todoItem.completed }"
+        @click="toggleComplete(todoItem.id)"
+    >
+        <span class="pointer" :class="{ 'todo-done': todoItem.completed }">
+            {{  todoItem.todo }} {{  todoItem.completed ? '(완료)' : '' }}
+        </span>
+        <span 
+            class="float-end badge bg-secondary pointer"
+            @click="deleteTodo(todoItem.id)"
+        >삭제</span>
+    </li>
+</template>
+<script setup>
+    const emit = defineEmits(['delete-todo', 'toggle-completed'])
+    const props = defineProps({
+        todoItem: {
+            type: Object,
+            required: true
+        }
+    })
+
+    const deleteTodo = (id) => {
+        console.log(id)
+        emit('delete-todo', id)
+    }
+
+    const toggleComplete = (id) =>{
+        emit('toggle-completed', id)
+    }
+</script>
