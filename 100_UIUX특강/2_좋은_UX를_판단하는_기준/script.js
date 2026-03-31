97b9663440b78cfcbc25d5a126afbaf56b36ef1e
kchun0513 <62457974+kchun0513@users.noreply.github.com>
2026-03-31 09:25:47 +0900

    docs: UI/UX 1일차 오후 수업 업로드

--- 100_UIUX특강/2_좋은_UX를_판단하는_기준/script.js
+++ 100_UIUX특강/2_좋은_UX를_판단하는_기준/script.js
@@ -16,7 +16,7 @@ function resetMessages() {
   // 메시지 영역 숨기기
   errorBox.classList.add("hidden");
   successBox.classList.add("hidden");
-  helperLink.classList.add("hidden");
+  findPw.classList.add("hidden");
 
   // 메시지 내용 초기화
   errorBox.textContent = "";
@@ -40,24 +40,20 @@ userId.addEventListener("keydown", (e) => {
 })
 
 function checkAuthentication() {
+  resetMessages();
   if (userPw.value == "" || userId.value == "") {
       errorBox.innerHTML = "<strong>ID와 패스워드를<br/>모두 입력해주세요.</strong>";
-      successBox.classList.add('hidden');
       errorBox.classList.remove("hidden");
       return;
     }
   else if (userPw.value == validPw && userId.value == validId) {
     successBox.innerHTML = "<strong>로그인에 성공하였습니다!</strong>";
     successBox.classList.remove('hidden');
-    errorBox.classList.add('hidden');
-    findPw.classList.add('hidden');
   } else if (userId.value != validId) {
     errorBox.innerHTML = "<strong>존재하지 않는 ID입니다.<br/>다시 입력해주세요.</strong>";
-    successBox.classList.add('hidden');
     errorBox.classList.remove("hidden");
   } else {
     errorBox.innerHTML = "<strong>비밀번호가 올바르지 않습니다.<br/>다시 입력해주세요.</strong>";
-    successBox.classList.add('hidden');
     findPw.classList.remove("hidden");
     errorBox.classList.remove("hidden");
   }
